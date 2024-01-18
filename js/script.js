24b41f178198c1cf289f1a64da13c7a0bc1c2e9e
AnuragA11 <techanurag1122@gmail.com>
2024-01-18 12:05:22 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// let a=prompt("What's your age? : ");
+// a=Number.parseInt(a);
+// if (a>0) {
+//     alert("This is a valid age")
+// }
+// else{
+//     alert("Invalid")
+// }
+
+// for (let i = 1; i<= 1000; i++) {
+//     document.write(i+ ")"+" HelloWorld<br>");
+    
+// }
+// for(let i=0; i<500; i++){
+//     document.write(i+1 + "<br>")
+// }
+
+// let obj = {
+//     a:100,
+//     b:90,
+//     c:80,
+//     d:70,
+//     e:60,
+//     f:50
+// }
+
+// for(let i in obj){
+//     document.write("Marks of " + i + " is " + obj[i]+ "<br>");
+// }
+
+// for (const b of "Anurag") {
+//     document.write(b+"<br>")
+// }
+
